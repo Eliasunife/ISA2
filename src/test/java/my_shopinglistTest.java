e0b11fb3967c43bda8a4592e31a4d8cf91e3cc71
Eliasunife <148888116+Eliasunife@users.noreply.github.com>
2023-11-09 10:08:17 +0100

    no

--- src/test/java/my_shopinglistTest.java
+++ src/test/java/my_shopinglistTest.java
@@ -8,16 +8,16 @@ public class my_shopinglistTest {
     @Test
     public void testAddItem() {
         my_shopinglist shoppingList = new my_shopinglist();
-        shoppingList.addItem("Milk");
-        assertTrue(shoppingList.viewItems().contains("Milk"));
+        shoppingList.addItem("Latte");
+        assertTrue(shoppingList.viewItems().contains("Latte"));
     }
 
     @Test
     public void testRemoveItem() {
         my_shopinglist shoppingList = new my_shopinglist();
-        shoppingList.addItem("Eggs");
-        shoppingList.removeItem("Eggs");
-        assertFalse(shoppingList.viewItems().contains("Eggs"));
+        shoppingList.addItem("Uova");
+        shoppingList.removeItem("Uova");
+        assertFalse(shoppingList.viewItems().contains("Uova"));
     }
 }
 
