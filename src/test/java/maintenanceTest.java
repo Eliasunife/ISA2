08675b399098a1b18873e22a59a585e36684c033
Eliasunife <148888116+Eliasunife@users.noreply.github.com>
2023-11-09 10:09:27 +0100

    yes

--- src/test/java/maintenanceTest.java
+++ src/test/java/maintenanceTest.java
@@ -7,15 +7,15 @@ public class maintenanceTest {
     @Test
     public void testAddActivity() {
         maintenance maintenanceClass = new maintenance();
-        maintenanceClass.addActivity("Oil Change");
-        assertTrue(maintenanceClass.checkUncompletedActivities().contains("Oil Change"));
+        maintenanceClass.addActivity("Cambio olio macchina");
+        assertTrue(maintenanceClass.checkUncompletedActivities().contains("cambio olio macchina"));
     }
 
     @Test
     public void testMarkActivityCompleted() {
         maintenance maintenanceClass = new maintenance();
-        maintenanceClass.addActivity("Fix Leak");
-        maintenanceClass.markActivityCompleted("Fix Leak");
-        assertFalse(maintenanceClass.checkUncompletedActivities().contains("Fix Leak"));
+        maintenanceClass.addActivity("sistemare il frigo");
+        maintenanceClass.markActivityCompleted("sistemare il frigo");
+        assertFalse(maintenanceClass.checkUncompletedActivities().contains("sistemare il frigo"));
     }
 }
